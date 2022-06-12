56f310b4af3904d0f5fd61dcc98d51b432281197
ApiProgrammer <ApiProgrammer@tutanota.com>
2022-06-12 19:43:13 -0300

    📦 Added BrickHill launcher

---
+++ launcher.rs
@@ -0,0 +1,11 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut arguments: Vec<String> = Vec::new();
+    for argument in std::env::args() { arguments.push(argument) }
+
+    std::process::Command::new("wine")
+                        .env("WINE_PROFILE", format!("{}/.brickhill/Player/", std::env::var("HOME")?))
+                        .arg(format!("{}/.brickhill/Player.exe", std::env::var("HOME")?))
+                        .arg(arguments[1].replace("brickhill.legacy://client/", ""))
+                        .spawn()?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,32 @@ use std::fs;
 use reqwest::blocking::get;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = "~/.brickhill";
-    let client = "~/.brickhill/Player.exe";
-    let playerprefix = "~/.brickhill/Player";
-    let workshopprefix = "~/.brickhill/Workshop";
+    let home = std::env::var("HOME")?;
+    let path = format!("{}/.brickhill/",home);
+    let client = format!("{}/.brickhill/Player.exe", home);
+    let launcher = format!("{}/.brickhill/launcher", home);
 
     fs::create_dir_all(path)?;
     let response = get("https://brkcdn.com/client/Player.exe")?.text()?;
     fs::write(client, response)?;
 
-    fs::write("~/.local/share/applications/brick-hill.desktop",
-    "[Desktop Entry]
+    fs::write(format!("{}/.local/share/applications/brick-hill.desktop", home),
+    format!("[Desktop Entry]
     Name=Brick Hill
     NoDisplay=true
-    Exec=$BRICK_HILL_PATH/launcher.sh %u
+    Exec={} %u
     Type=Application
     Terminal=false
-    MimeType=x-scheme-handler/brickhill.legacy;")?;
+    MimeType=x-scheme-handler/brickhill.legacy;", &launcher))?;
 
     Command::new("xdg-mime")
         .arg("default")
         .arg("brick-hill.desktop")
         .arg("x-scheme-handler/brickhill.legacy")
         .spawn()?;
+
+    
+
+    println!("Finished installation!");
     Ok(())
 }
