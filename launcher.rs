e76d98d9392cf41d8d6d0f125f687a1d29316873
ApiProgrammer <ApiProgrammer@tutanota.com>
2022-06-13 11:21:54 -0300

    🚀 Fixed Player.exe not installing correctly and improved file size by 92%

--- launcher.rs
+++ launcher.rs
@@ -3,7 +3,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for argument in std::env::args() { arguments.push(argument) }
 
     std::process::Command::new("wine")
-                        .env("WINE_PROFILE", format!("{}/.brickhill/Player/", std::env::var("HOME")?))
+                        .env("WINEPROFILE", format!("{}/.brickhill/Player/", std::env::var("HOME")?))
                         .arg(format!("{}/.brickhill/Player.exe", std::env::var("HOME")?))
                         .arg(arguments[1].replace("brickhill.legacy://client/", ""))
                         .spawn()?;
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
 use std::process::Command;
 use std::fs;
-use reqwest::blocking::get;
+use std::io::Write;
+use curl::easy::Easy;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut reqclient = Easy::new();
     let home = std::env::var("HOME")?;
-    let path = format!("{}/.brickhill/",home);
+    let path = format!("{}/.brickhill/Player/",home);
     let client = format!("{}/.brickhill/Player.exe", home);
     let launcher = format!("{}/.brickhill/launcher", home);
 
     fs::create_dir_all(path)?;
-    let clientbody = get("https://brkcdn.com/client/Player.exe")?.text()?;
-    fs::write(client, clientbody)?;
+    let mut clientfile = fs::File::create(client)?;
+    reqclient.url("https://brkcdn.com/client/Player.exe")?;
+    reqclient.write_function(move |data| {
+        clientfile.write_all(data).unwrap();
+        Ok(data.len())
+    })?;
+    reqclient.perform()?;
 
     fs::write(format!("{}/.local/share/applications/brick-hill.desktop", home),
     format!("[Desktop Entry]
-    Name=Brick Hill
-    NoDisplay=true
-    Exec={} %u
-    Type=Application
-    Terminal=false
-    MimeType=x-scheme-handler/brickhill.legacy;", &launcher))?;
+Name=Brick Hill
+NoDisplay=true
+Exec={} %u
+Type=Application
+Terminal=false
+MimeType=x-scheme-handler/brickhill.legacy;", &launcher))?;
 
     Command::new("xdg-mime")
         .arg("default")
@@ -27,8 +34,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .arg("x-scheme-handler/brickhill.legacy")
         .spawn()?;
 
-    let launcherbody = get("https://raw.githubusercontent.com/brickhill-community/Beverage/main/launcher")?.text()?;
-    fs::write(launcher, launcherbody)?;
+    let mut launcherfile = fs::File::create(&launcher)?;
+    reqclient.url("https://brkcdn.com/client/Player.exe")?;
+    reqclient.write_function(move |data| {
+        launcherfile.write_all(data).unwrap();
+        Ok(data.len())
+    })?;
+    reqclient.perform()?;
+
+    Command::new("chmod")
+            .arg("+x")
+            .arg(launcher)
+            .spawn()?;
 
     println!("Finished installation!");
     Ok(())
