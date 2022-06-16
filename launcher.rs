125b81fbb46bfcd6cdaf13cde77eb4fe3c25739e
ApiProgrammer <ApiProgrammer@tutanota.com>
2022-06-16 15:06:02 -0300

    👍 Improve the launcher by a bit

--- launcher.rs
+++ launcher.rs
@@ -1,11 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut arguments: Vec<String> = Vec::new();
-    for argument in std::env::args() { arguments.push(argument) }
-
     std::process::Command::new("wine")
-                        .env("WINEPROFILE", format!("{}/.brickhill/Player/", std::env::var("HOME")?))
-                        .arg(format!("{}/.brickhill/Player.exe", std::env::var("HOME")?))
-                        .arg(arguments[1].replace("brickhill.legacy://client/", ""))
-                        .spawn()?;
+                         .env("WINEPROFILE", format!("{}/.brickhill/Player/", std::env::var("HOME")?))
+                         .args([ format!("{}/.brickhill/Player.exe", std::env::var("HOME"))?,
+                                 std::env::args().collect::<Vec<String>>()[1].replace("brickhill.legacy://client/", "")
+                              ])
+                         .spawn()?;
     Ok(())
 }
